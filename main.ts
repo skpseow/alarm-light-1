ab6d43c283959eb832c7236515f75ccb53802e90
skpseow <17407691+skpseow@users.noreply.github.com>
2020-10-20 16:26:06 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onPinPressed(TouchPin.P0, function () {
     for (let index = 0; index < 5; index++) {
         pins.digitalWritePin(DigitalPin.P1, 1)
         basic.pause(500)
-        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
         basic.pause(500)
     }
 })
